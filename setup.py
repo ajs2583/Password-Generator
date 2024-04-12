634f2f9158140ff9468f704d444aab1b27e590bf
Andrew Sliva <148899989+ajs2583@users.noreply.github.com>
2024-04-12 12:41:10 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='Password-Generator',
+      version='0.1',
+      description='Python - Random Password Generator ( R.P.G. )',
+      url='https://github.com/ajs2583/Password-Generator',
+      author='Andrew Sliva',
+      author_email='sliva.andrew1502@gmail.com',
+      license='MIT',
+      packages=['source'],
+      python_requires=">=3",
+      zip_safe=False)
